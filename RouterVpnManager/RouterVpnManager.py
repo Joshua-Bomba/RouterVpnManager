32206b4217b6ac439dd12a45b6ef2b5e7c519eaa
Joshua Bomba <snipy67@hotmail.com>
2018-07-22 01:01:16 -0400

    #whoops forgot to start it https://youtu.be/KIrCOfDbL_E?t=1m44s

--- RouterVpnManager/RouterVpnManager.py
+++ RouterVpnManager/RouterVpnManager.py
@@ -31,6 +31,7 @@ class logger(threading.Thread):
     def __init__(self):
         self.__output = True
         self.__outputQueue = Queue.Queue()
+        self.start()#whoops forgot to start it https://youtu.be/KIrCOfDbL_E?t=1m44s
     def setConnections(self,connections):
         self.__connections = connections
     def write(self,outputString):
