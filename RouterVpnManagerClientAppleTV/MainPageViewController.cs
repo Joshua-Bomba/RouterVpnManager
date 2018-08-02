3844b43993cbe111077517c637b0b1213f966593
Joshua Bomba <snipy67@hotmail.com>
2018-08-01 21:43:21 -0400

    adjusted the layout a bit on the mainpage

---
+++ RouterVpnManagerClientAppleTV/MainPageViewController.cs
@@ -0,0 +1,28 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace RouterVpnManagerClient
+{
+    public partial class MainPageViewController : UIViewController
+    {
+        public MainPageViewController (IntPtr handle) : base (handle)
+        {
+
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            btnStack.Axis = UILayoutConstraintAxis.Vertical;
+            btnStack.Alignment = UIStackViewAlignment.Fill;
+            btnStack.Distribution = UIStackViewDistribution.EqualSpacing;
+            btnStack.Spacing = 10;
+            btnStack.TranslatesAutoresizingMaskIntoConstraints = false;
+
+        }
+
+        
+    }
+}
