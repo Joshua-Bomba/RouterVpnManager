ccd5b2b9ac3390bf1bc0d4239fdb36c4a049b8d0
Joshua Bomba <snipy67@hotmail.com>
2018-08-08 21:26:29 -0400

    added a setting view

---
+++ RouterVpnManagerClientAppleTV/UIAndPortPageController.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace RouterVpnManagerClient
+{
+    public partial class UIAndPortPageController : UIViewController
+    {
+        public UIAndPortPageController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
